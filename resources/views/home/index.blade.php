5fb8579977609e71c66cb857dafac9cf4c07b2ba
韩剑 <hanjian@xgo.cn>
2018-11-16 14:27:44 +0800

    前台页面

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -5,8 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>首页</title>
+    <link id="skin-link" href="{{asset('home/css/index.css')}}" rel="stylesheet" type="text/css" />
 </head>
 <body>
     ddddd
+    
+<script src="{{asset('home/js/index.js')}}"></script>
 </body>
 </html>
